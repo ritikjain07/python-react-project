6ed67e8cdcd1a0ebf7f669d7d1d7b9f6b1db5694
ritikjain07 <ritikjain4560@gmail.com>
2025-03-11 22:28:08 +0530

    add backend routes for serving frontend files and create WSGI entry point; update Vite config to use Tailwind CSS

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -10,12 +11,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
+dist_folder = os.path.join(frontend_folder, "dist")
+
 # Import models
 from models import Friend
 
 # Import and register blueprints
 from routes import friends_bp
 app.register_blueprint(friends_bp)
+
+@app.route("/", defaults={'filename': ""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder, filename)
 
 with app.app_context():
     db.create_all()
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,25 @@
-/frontend/vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react()
+//   ],
+//   darkMode: 'class',
+//   server: {
+//     port: 3000
+//   }
+// })
+
+
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
-    react()
+    tailwindcss(),
   ],
-  darkMode: 'class',
   server: {
     port: 3000
   }
